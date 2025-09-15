f828348ec45e43e2eb5f9bb2df36b6e36812ebbd
Nolen Johnson <johnsonnolen@gmail.com>
2025-09-15 03:01:00 -0400

    Revert "Temporarily drop MotCamera4"
    This reverts commit 8fe01d022869d080f9d1f594959c1fb3e9fd269d.
    
    Reason: Fixed.

--- berlin-vendor.mk
+++ berlin-vendor.mk
@@ -3405,6 +3405,7 @@ PRODUCT_PACKAGES += \
     MotCamera3AI \
     HotwordEnrollmentOKGoogleHEXAGON_WIDEBAND \
     HotwordEnrollmentXGoogleHEXAGON_WIDEBAND \
+    MotCamera4 \
     MotorolaSettingsProvider \
     com.motorola.motosignature \
     moto-core_services \
